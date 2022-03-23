b35f6aea03cb31af45f23c5dc5b5054b11901e1b
devilgoth <nycolasinepsy@gmail.com>
2022-03-23 17:44:04 -0300

    setup: with react-query

---
+++ src/pages/Repos.tsx
@@ -0,0 +1,39 @@
+import { useQuery } from 'react-query'
+import axios from 'axios'
+import {Link} from 'react-router-dom'
+
+
+export type Repository = {
+  full_name: string;
+  description: string;
+}
+
+
+export function Repos() {
+
+  const { data, isFetching } = useQuery<Repository[]>('repos', async () => {
+    const response = await axios.get('https://api.github.com/users/nicolasecurity/repos');
+
+    return response.data;
+  }, {
+      staleTime: 1000 * 60, // 1 minute
+  })
+
+  return (
+    <ul>
+      {isFetching && <p>Carregando...</p>}
+      {data?.map(repo => {
+        return( 
+          <li key={repo.full_name}>
+            <Link to={`repos/${repo.full_name}`}>
+              {repo.full_name}
+            </Link>
+            <p>
+              {repo.description}
+            </p>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
